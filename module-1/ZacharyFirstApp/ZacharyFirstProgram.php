35908f491ffcd29aa49374219641f1ccd0999e5b
SilentRegent <zacharyanderson0423@gmail.com>
2026-03-23 21:57:00 -0700

    Pushing Assignment 1.3 3/23/26

---
+++ module-1/ZacharyFirstApp/ZacharyFirstProgram.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <!--
+      Zachary Anderson
+      3/23/26
+      Assignment 1.3
+      Hello World PHP Program
+     -->
+  <head>
+
+    <title> PHP Snippets </title>
+    <meta charset='utf-8'>
+
+  </head>
+
+  <body>
+
+    <?php
+        # PHP script section start
+      echo("Hello World <br />");
+
+    ?>
+        
+    <?php
+        # Second PHP script section start
+        echo("This is my first PHP program! <br />");
+    ?>
+    
+  </body>
+
+</html>
